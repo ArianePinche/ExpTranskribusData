929f8089e1920d646561d7b25d29efd8390de654
ArianePinche <ariane.pinche@gmail.com>
2021-12-05 21:01:32 +0100

    Mise en place entrainement

--- EntrainementKraken.sh
+++ EntrainementKraken.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-echo "Training eScriptorium.normal"
+echo "Training eScriptorium"
 ketos train  -r 0.0001 --lag 20\
  --preload -f alto -e escriptorium.dev.txt -t escriptorium.train.txt -u NFKD --device cuda:0 --batch-size 32 -o eScriptorium.normal --threads 10
 
 
-echo "Training transkribus.normal"
+echo "Training transkribus_SimpleNoRepoly"
 ketos train  -r 0.0001 --lag 20\
---preload -f alto -e transkribus.dev.txt -t transkribus.train.txt -u NFKD --device cuda:0 --batch-size 32 -o transkribus.normal --threads 10
+--preload -f alto -e TranskribusSimpleMask_nopoly_dev.txt -t TranskribusSimpleMask_nopoly_train.txt -u NFKD --device cuda:0 --batch-size 32 -o transkribus_SimpleNoRepoly --threads 10
 
 
-echo "Training transkribus.repolygonize"
+echo "Training transkribus_SimpleRepoly"
 ketos train  -r 0.0001 --lag 20\
- --preload -f alto -e transkribus.dev.txt -t transkribus.train.txt -u NFKD --device cuda:0 --batch-size 32 -o transkribus.repolygonize --threads 10 --repolygonize 
+ --preload -f alto -e TranskribusSimpleMask_poly_dev.txt -t TranskribusSimpleMask_poly_train.txt -u NFKD --device cuda:0 --batch-size 32 -o transkribus_SimpleRepoly --threads 10
+
+
+echo "Training transkribus_ComplexNoRepoly"
+ketos train  -r 0.0001 --lag 20\
+--preload -f alto -e TranskribusComplexMask_nopoly_dev.txt -t TranskribusComplexMask_nopoly_train.txt -u NFKD --device cuda:0 --batch-size 32 -o transkribus_ComplexNoRepoly --threads 10
+
+echo "Training transkribus_ComplexRepoly"
+ketos train  -r 0.0001 --lag 20\
+ --preload -f alto -e TranskribusComplexMask_poly_dev.txt -t TranskribusComplexMask_poly_train.txt -u NFKD --device cuda:0 --batch-size 32 -o transkribus_ComplexRepoly --threads 10
